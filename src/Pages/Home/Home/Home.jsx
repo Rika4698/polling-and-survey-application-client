1e451ac02a3078eb7fcf7ebc5c2290ee1a97f215
Rika4698 <rikasharmin181@gmail.com>
2023-11-25 19:23:40 +0600

    create testimonials

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Testimonial from "../Testimonials/Testimonial";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Testimonial></Testimonial>
         </div>
     );
 };
