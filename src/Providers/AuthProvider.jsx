4faf379108721cedefc1ae43356bbbaa44829ebd
Rika4698 <rikasharmin181@gmail.com>
2023-11-26 17:32:49 +0600

    store and remove token

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -5,6 +5,7 @@ import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
 
 import{createUserWithEmailAndPassword,updateProfile,getAuth,onAuthStateChanged,signInWithEmailAndPassword,signOut,GoogleAuthProvider,signInWithPopup} from "firebase/auth";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 
 
 export const AuthContext = createContext(null);
@@ -16,7 +17,7 @@ const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null);
     const [loading,setLoading] = useState(true);
-   
+   const axiosPublic = useAxiosPublic();
     const createUser = (displayName, photoURL,email,password)=>{
         setLoading(true);
 
@@ -54,6 +55,18 @@ const AuthProvider = ({children}) => {
     
      
      setUser(currentUser);
+     if(currentUser){
+        const userInfo ={email:currentUser.email};
+        axiosPublic.post('/jwt', userInfo)
+            .then(res => {
+                if(res.data.token){
+                    localStorage.setItem('access-token',res.data.token);
+                }
+            })
+     }
+     else{
+        localStorage.removeItem('access-token');
+     }
      setLoading(false);
      });
     
